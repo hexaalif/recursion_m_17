6c63f748087dd3b52a1373eb1582138a77d340fa
hexaalif <hexaalif2020@gmail.com>
2023-05-10 05:56:16 +0600

    file added

--- print_reverse.c
+++ print_reverse.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+void prnt(int i)
+{
+    // i--;
+    if (i == 0)
+        return;
+    printf("%d\n", i);
+    prnt(i - 1);
+}
+
 int main()
 {
-
+    prnt(5);
     return 0;
 }
